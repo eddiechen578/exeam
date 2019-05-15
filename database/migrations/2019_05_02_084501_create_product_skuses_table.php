038e619d8a3da0a203135b57e7f622c6d7cde82f
eddiechen <eddiechen578@gmail.com>
2019-05-15 17:11:31 +0800

    update

---
+++ database/migrations/2019_05_02_084501_create_product_skuses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductSkusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_skuses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price', 10, 0);
+            $table->unsignedInteger('stock');
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')
+                  ->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_skuses');
+    }
+}
